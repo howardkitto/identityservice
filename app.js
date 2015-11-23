ee1d9b6f15133dbf3dfba6e6653643cda0bcbab8
kitto.howard@gmail.com <kitto.howard@gmail.com>
2015-11-23 10:39:34 +0000

    trying to update origin

--- app.js
+++ app.js
@@ -2,35 +2,19 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
+var mongoose = require('mongoose');
 
-//==================================================================
-// Define the strategy to be used by PassportJS
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    if (username === "admin" && password === "admin") // stupid example
-      return done(null, {name: "admin"});
+var Identity = require('./models/identity');
+var configDB = require('./config/database.js');
 
-    return done(null, false, { message: 'Incorrect username.' });
+mongoose.connect(configDB.url, function (err, res) {
+  if (err) {
+  console.log ('ERROR connecting to: ' + configDB.url + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + configDB.url);
   }
-));
-
-// Serialized and deserialized methods when got from session
-passport.serializeUser(function(user, done) {
-    done(null, user);
 });
 
-passport.deserializeUser(function(user, done) {
-    done(null, user);
-});
-
-// Define a middleware function to be used for every secured routes
-var auth = function(req, res, next){
-  if (!req.isAuthenticated()) 
-  	res.send(401);
-  else
-  	next();
-};
 //==================================================================
 
 // Start express application
@@ -40,11 +24,13 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
+require('./config/passport')(passport, Identity);
+
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.cookieParser()); 
 app.use(express.bodyParser());
-app.use(express.methodOverride());
 app.use(express.session({ secret: 'securedsession' }));
 app.use(passport.initialize()); // Add passport initialization
 app.use(passport.session());    // Add passport initialization
@@ -56,34 +42,7 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-//==================================================================
-// routes
-app.get('/', function(req, res){
-  res.render('index', { title: 'Express' });
-});
-
-app.get('/users', auth, function(req, res){
-  res.send([{name: "user1"}, {name: "user2"}]);
-});
-//==================================================================
-
-//==================================================================
-// route to test if the user is logged in or not
-app.get('/loggedin', function(req, res) {
-  res.send(req.isAuthenticated() ? req.user : '0');
-});
-
-// route to log in
-app.post('/login', passport.authenticate('local'), function(req, res) {
-  res.send(req.user);
-});
-
-// route to log out
-app.post('/logout', function(req, res){
-  req.logOut();
-  res.send(200);
-});
-//==================================================================
+require('./app/routes.js')(app, passport, Identity);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
